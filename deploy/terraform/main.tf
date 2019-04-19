e5f4d2702616e23166ad07acfb58166395f62bf3
Caio Zem <caioadz@hotmail.com>
2019-04-19 18:48:24 -0300

    Consuming environment variables

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -2,11 +2,7 @@ terraform {
   required_version = ">= 0.11.13"
 }
 
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
-}
+provider "aws" {}
 
 module "ecr_repository" {
   source = "./modules/ecr-repository"
--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,13 +1,4 @@
 variable "service_name" {
   description = "An unique name that identifies your service. This will be used as prefix for many AWS resources."
-}
-
-variable "aws_access_key" {
-}
-
-variable "aws_secret_key" {
-}
-
-variable "aws_region" {
-  default = "us-east-1"
+  default = "sharebook"
 }
