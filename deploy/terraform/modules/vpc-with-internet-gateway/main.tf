60d88f143b16cf2b63ce5e783fa2c28fc9c4fabd
Caio Zem <caioadz@hotmail.com>
2019-04-20 16:57:49 -0300

    Moving submodules inside

--- deploy/terraform/modules/vpc-with-internet-gateway/main.tf
+++ deploy/terraform/modules/vpc-with-internet-gateway/main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source = "../vpc"
+  source = "./modules/vpc"
 
   name = "${var.vpc_name}"
   cidr_block = "${var.cidr_block}"
 }
 
 module "internet_gateway" {
-  source = "../internet-gateway"
+  source = "./modules/internet-gateway"
 
   name = "${var.internet_gateway_name}"
   vpc_id = "${module.vpc.id}"
