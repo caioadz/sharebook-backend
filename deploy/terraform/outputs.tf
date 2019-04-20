31ef0a4fade9cea1107bfc66acab110fe64a494e
Caio Zem <caioadz@hotmail.com>
2019-04-20 16:08:09 -0300

    Updating outputs

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -2,14 +2,17 @@ output "ecr_repository_arn" {
   value = "${module.ecr_repository.arn}"
 }
 
-output "ecr_repository_name" {
-  value = "${module.ecr_repository.name}"
-}
-
-output "ecr_repository_registry_id" {
-  value = "${module.ecr_repository.registry_id}"
-}
-
-output "ecr_repository_repository_url" {
+output "ecr_repository_url" {
   value = "${module.ecr_repository.repository_url}"
+}
+output "vpc_arn" {
+  value = "${module.vpc_with_internet_gateway.arn}"
+}
+
+output "vpc_id" {
+  value = "${module.vpc_with_internet_gateway.vpc_id}"
+}
+
+output "internet_gateway_id" {
+  value = "${module.vpc_with_internet_gateway.internet_gateway_id}"
 }
