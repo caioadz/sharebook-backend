9acf22133516df90099e75039fbf89403ea1fe11
Caio Zem <caioadz@hotmail.com>
2019-04-20 16:08:04 -0300

    Creating VPC with IG

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -9,3 +9,12 @@ module "ecr_repository" {
 
   repository_name = "${var.service_name}"
 }
+
+module "vpc_with_internet_gateway" {
+  source = "./modules/vpc-with-internet-gateway"
+
+  vpc_name = "${var.service_name}-prod"
+  cidr_block = "10.0.0.0/16"
+
+  internet_gateway_name = "${var.service_name}-prod"
+}
