7e1f28e02e730ec0ce3d715c5364e90599445c6d
Caio Zem <caioadz@hotmail.com>
2019-04-20 16:56:00 -0300

    Fixing variable name

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -6,7 +6,7 @@ output "ecr_repository_url" {
   value = "${module.ecr_repository.repository_url}"
 }
 output "vpc_arn" {
-  value = "${module.vpc_with_internet_gateway.arn}"
+  value = "${module.vpc_with_internet_gateway.vpc_arn}"
 }
 
 output "vpc_id" {
