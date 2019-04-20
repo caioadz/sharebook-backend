20d132ca18de7efc2a67c02c11f5055e9ee0df80
Caio Zem <caioadz@hotmail.com>
2019-04-20 15:50:12 -0300

    Creating Internet Gateway module

---
+++ deploy/terraform/modules/internet-gateway/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {
+  description = "Name tag of the Internet Gateway that will be created."
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC which this Internet Gateway will be attached to."
+}
