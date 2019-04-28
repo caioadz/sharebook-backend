ddc932cfebbc32a11e4b271c61131f700d87e6b8
Caio Zem <caioadz@hotmail.com>
2019-04-28 11:14:18 -0300

    Configuring default IGW route

--- deploy/terraform/modules/vpc-with-internet-gateway/main.tf
+++ deploy/terraform/modules/vpc-with-internet-gateway/main.tf
@@ -11,3 +11,9 @@ module "internet_gateway" {
   name = "${var.internet_gateway_name}"
   vpc_id = "${module.vpc.id}"
 }
+
+resource "aws_route" "igw_route" {
+  route_table_id         = "${module.vpc.default_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${module.internet_gateway.id}"
+}
--- deploy/terraform/modules/vpc-with-internet-gateway/modules/vpc/outputs.tf
+++ deploy/terraform/modules/vpc-with-internet-gateway/modules/vpc/outputs.tf
@@ -10,6 +10,10 @@ output "cidr_block" {
   value = "${aws_vpc.vpc.cidr_block}"
 }
 
+output "default_route_table_id" {
+  value = "${aws_vpc.vpc.default_route_table_id}"
+}
+
 output "default_security_group_id" {
   value = "${aws_vpc.vpc.default_security_group_id}"
 }
