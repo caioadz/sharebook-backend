ac10e1be97520468ae61987150302a0679ff3c9e
Caio Zem <caioadz@hotmail.com>
2019-05-01 18:52:04 -0300

    Implementing computed ingress/egress
    Count does not support computed values, we need to specify the number of items in the list manually.

--- deploy/terraform/modules/ecs-cluster/modules/security-group/main.tf
+++ deploy/terraform/modules/ecs-cluster/modules/security-group/main.tf
@@ -15,6 +15,9 @@ resource "aws_security_group_rule" "default_egress_rule" {
     security_group_id = "${aws_security_group.security_group.id}"
 }
 
+###################################
+# INGRESS
+###################################
 
 resource "aws_security_group_rule" "ingress_with_cidr_blocks" {
   count = "${length(var.ingress_with_cidr_blocks)}"
@@ -42,6 +45,10 @@ resource "aws_security_group_rule" "ingress_with_source_security_group_id" {
   security_group_id = "${aws_security_group.security_group.id}"
 }
 
+###################################
+# EGRESS
+###################################
+
 resource "aws_security_group_rule" "egress_with_cidr_blocks" {
   count = "${length(var.egress_with_cidr_blocks)}"
 
@@ -64,6 +71,66 @@ resource "aws_security_group_rule" "egress_with_source_security_group_id" {
   protocol  = "${lookup(var.egress_with_source_security_group_id[count.index], "protocol")}"
 
   source_security_group_id = "${lookup(var.egress_with_source_security_group_id[count.index], "source_security_group_id")}"
+
+  security_group_id = "${aws_security_group.security_group.id}"
+}
+
+###################################
+# COMPUTED INGRESS
+###################################
+
+resource "aws_security_group_rule" "computed_ingress_with_cidr_blocks" {
+  count = "${var.computed_ingress_with_cidr_blocks_count}"
+
+  type      = "ingress"
+  from_port = "${lookup(var.computed_ingress_with_cidr_blocks[count.index], "from_port")}"
+  to_port   = "${lookup(var.computed_ingress_with_cidr_blocks[count.index], "to_port")}"
+  protocol  = "${lookup(var.computed_ingress_with_cidr_blocks[count.index], "protocol")}"
+
+  cidr_blocks = ["${lookup(var.computed_ingress_with_cidr_blocks[count.index], "cidr_blocks")}"]
+
+  security_group_id = "${aws_security_group.security_group.id}"
+}
+
+resource "aws_security_group_rule" "computed_ingress_with_source_security_group_id" {
+  count = "${var.computed_ingress_with_source_security_group_id_count}"
+
+  type      = "ingress"
+  from_port = "${lookup(var.computed_ingress_with_source_security_group_id[count.index], "from_port")}"
+  to_port   = "${lookup(var.computed_ingress_with_source_security_group_id[count.index], "to_port")}"
+  protocol  = "${lookup(var.computed_ingress_with_source_security_group_id[count.index], "protocol")}"
+
+  source_security_group_id = "${lookup(var.computed_ingress_with_source_security_group_id[count.index], "source_security_group_id")}"
+
+  security_group_id = "${aws_security_group.security_group.id}"
+}
+
+###################################
+# COMPUTED EGRESS
+###################################
+
+resource "aws_security_group_rule" "computed_egress_with_cidr_blocks" {
+  count = "${var.computed_egress_with_cidr_blocks_count}"
+
+  type      = "egress"
+  from_port = "${lookup(var.computed_egress_with_cidr_blocks[count.index], "from_port")}"
+  to_port   = "${lookup(var.computed_egress_with_cidr_blocks[count.index], "to_port")}"
+  protocol  = "${lookup(var.computed_egress_with_cidr_blocks[count.index], "protocol")}"
+
+  cidr_blocks = ["${lookup(var.computed_egress_with_cidr_blocks[count.index], "cidr_blocks")}"]
+
+  security_group_id = "${aws_security_group.security_group.id}"
+}
+
+resource "aws_security_group_rule" "computed_egress_with_source_security_group_id" {
+  count = "${var.computed_egress_with_source_security_group_id_count}"
+
+  type      = "egress"
+  from_port = "${lookup(var.computed_egress_with_source_security_group_id[count.index], "from_port")}"
+  to_port   = "${lookup(var.computed_egress_with_source_security_group_id[count.index], "to_port")}"
+  protocol  = "${lookup(var.computed_egress_with_source_security_group_id[count.index], "protocol")}"
+
+  source_security_group_id = "${lookup(var.computed_egress_with_source_security_group_id[count.index], "source_security_group_id")}"
 
   security_group_id = "${aws_security_group.security_group.id}"
 }
--- deploy/terraform/modules/ecs-cluster/modules/security-group/variables.tf
+++ deploy/terraform/modules/ecs-cluster/modules/security-group/variables.tf
@@ -10,6 +10,10 @@ variable "vpc_id" {
   description = "ID of the VPC where this Security Group will be created."
 }
 
+###################################
+# INGRESS
+###################################
+
 variable "ingress_with_cidr_blocks" {
   type        = "list"
   description = "List of the ingress rules by CIDR blocks."
@@ -22,6 +26,10 @@ variable "ingress_with_source_security_group_id" {
   default     = []
 }
 
+###################################
+# EGRESS
+###################################
+
 variable "egress_with_cidr_blocks" {
   type        = "list"
   description = "List of the egress rules by CIDR blocks."
@@ -32,4 +40,64 @@ variable "egress_with_source_security_group_id" {
   type        = "list"
   description = "List of the egress rules by Security Group ID."
   default     = []
+}
+
+###################################
+# COMPUTED INGRESS
+###################################
+
+variable "computed_ingress_with_cidr_blocks" {
+  type        = "list"
+  description = "List of the ingress rules by CIDR blocks."
+  default     = []
+}
+
+variable "computed_ingress_with_source_security_group_id" {
+  type        = "list"
+  description = "List of the ingress rules by Security Group ID."
+  default     = []
+}
+
+###################################
+# NUMBER OF COMPUTED INGRESS
+###################################
+
+variable "computed_ingress_with_cidr_blocks_count" {
+  description = "Number of computed ingress rules by CIDR blocks."
+  default     = 0
+}
+
+variable "computed_ingress_with_source_security_group_id_count" {
+  description = "Number of computed ingress rules by Security Group ID."
+  default     = 0
+}
+
+###################################
+# COMPUTED EGRESS
+###################################
+
+variable "computed_egress_with_cidr_blocks" {
+  type        = "list"
+  description = "List of the egress rules by CIDR blocks."
+  default     = []
+}
+
+variable "computed_egress_with_source_security_group_id" {
+  type        = "list"
+  description = "List of the egress rules by Security Group ID."
+  default     = []
+}
+
+###################################
+# NUMBER OF COMPUTED EGRESS
+###################################
+
+variable "computed_egress_with_cidr_blocks_count" {
+  description = "Number of computed egress rules by CIDR blocks."
+  default     = 0
+}
+
+variable "computed_egress_with_source_security_group_id_count" {
+  description = "Number of computed egress rules by Security Group ID."
+  default     = 0
 }
