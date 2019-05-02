7cb5ecc987a7cf8c86249032ab11ce387e6ccfd2
Caio Zem <caioadz@hotmail.com>
2019-05-02 09:08:31 -0300

    Adding instance_profile_name to the output

--- deploy/terraform/modules/ecs-cluster/modules/default-instance-role/outputs.tf
+++ deploy/terraform/modules/ecs-cluster/modules/default-instance-role/outputs.tf
@@ -6,6 +6,10 @@ output "name" {
   value = "${aws_iam_role.iam_role.name}"
 }
 
+output "instance_profile_name" {
+  value = "${aws_iam_instance_profile.iam_instance_profile.name}"
+}
+
 output "ssm_policy_arn" {
   value = "${aws_iam_role_policy_attachment.attach_ssm_policy.policy_arn}"
 }
