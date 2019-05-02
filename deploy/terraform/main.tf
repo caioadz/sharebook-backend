5fbe61d6ad48331cfdb16f1b838077fe204243e3
Caio Zem <caioadz@hotmail.com>
2019-05-02 09:09:53 -0300

    Updating main example

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
 
 provider "aws" {}
 
+locals {
+  prefix = "${var.service_name}-${var.environment}"
+}
+
 module "ecr_repository" {
   source = "./modules/ecr-repository"
 
@@ -13,16 +17,16 @@ module "ecr_repository" {
 module "vpc_with_internet_gateway" {
   source = "./modules/vpc-with-internet-gateway"
 
-  vpc_name   = "${var.service_name}-prod"
+  vpc_name   = "${local.prefix}"
   cidr_block = "10.0.0.0/16"
 
-  internet_gateway_name = "${var.service_name}-prod"
+  internet_gateway_name = "${local.prefix}"
 }
 
 module "public_private_multiaz_subnets" {
   source = "./modules/public-private-multiaz-subnets"
 
-  prefix = "${var.service_name}-prod"
+  prefix = "${local.prefix}"
   vpc_id = "${module.vpc_with_internet_gateway.vpc_id}"
 
   availability_zone_count = 2
@@ -31,6 +35,9 @@ module "public_private_multiaz_subnets" {
 module "ecs_cluster" {
   source = "./modules/ecs-cluster"
 
-  prefix = "${var.service_name}"
+  prefix = "${local.prefix}"
   vpc_id = "${module.vpc_with_internet_gateway.vpc_id}"
+
+  image_id = "${var.image_id}"
+  instance_type = "${var.instance_type}"
 }
--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -2,3 +2,18 @@ variable "service_name" {
   description = "An unique name that identifies your service. This will be used as prefix for many AWS resources."
   default     = "sharebook"
 }
+
+variable "environment" {
+  description = "Environment name."
+  default     = "prod"
+}
+
+variable "image_id" {
+  description = "ID of the AMI which will be used to create your ECS instances."
+  default     = "ami-0be9e1908fe51a590"
+}
+
+variable "instance_type" {
+  description = "Instance type which will be used to created your ECS instances."
+  default     = "t2.micro"
+}
