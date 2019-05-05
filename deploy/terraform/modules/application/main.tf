ca7b8d32e549efb361e42b9899d5433737a16373
Caio Zem <caioadz@hotmail.com>
2019-05-05 07:43:09 -0300

    App. module receive these variables temporarily

--- deploy/terraform/modules/application/main.tf
+++ deploy/terraform/modules/application/main.tf
@@ -12,13 +12,13 @@ module "load_balancer" {
   source = "./modules/load-balancer"
   
   name = "${local.prefix}"
-  #security_groups = ["${module.alb_security_group.id}"]
-  #subnet_ids = "${var.lb_subnets}"
+  security_groups = "${var.lb_security_group_ids}"
+  subnet_ids = "${var.lb_subnet_ids}"
 
   target_type = "instance"
   target_port = 80
   target_protocol = "HTTP"
-  #vpc_id = "${var.vpc_id}"
+  vpc_id = "${var.vpc_id}"
 
   listeners = [
     {
--- deploy/terraform/modules/application/variables.tf
+++ deploy/terraform/modules/application/variables.tf
@@ -5,3 +5,21 @@ variable "service_name" {
 variable "environment" {
   description = "Environment name."
 }
+
+############################################################################
+# VARIABLES THAT SHOULD BE REFACTORED INTO DATASOURCES FILTERING FROM TAGS
+############################################################################
+
+variable "vpc_id" {
+  description = "ID of the VPC where your resources will be placed."
+}
+
+variable "lb_security_group_ids" {
+  type        = "list"
+  description = "Security group where your Load Balancer should be placed."
+}
+
+variable "lb_subnet_ids" {
+  type        = "list"
+  description = "ID of the subnets where you want to place your load balancer."
+}
